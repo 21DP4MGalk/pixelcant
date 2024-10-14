8bab4eaf180984c0654f7c10fb6d6da0166f3df4
21DP4KLogi <103188972+21DP4KLogi@users.noreply.github.com>
2024-10-14 13:54:13 +0000

    Fixed the new backend setup script

--- createBackend.sh
+++ createBackend.sh
@@ -1,11 +1,11 @@
 if [ ! -f .env ]; then
-    echo "DB_HOST=localhost
+    echo "DB_HOST=postgres
 DB_USER=postgres
 DB_PASS=hunter2
 DB_NAME=pixelcant
 DB_STARTUP_TIMEOUT=5" > .env
 fi
 
-docker compose -f utils/compose.yaml --project-directory . up -d
+docker compose -f utils/compose.yaml --project-directory . up -d --build
 
 echo "Server is up! Write 'docker compose down' to shut it down"
