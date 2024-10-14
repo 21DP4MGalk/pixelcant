573120e20bb2a7c70adf74da42049cc4c06c5440
21DP4MGalk <103188871+21DP4MGalk@users.noreply.github.com>
2024-10-14 19:38:10 +0000

    Added simplified navbar for convenience and added front end support for chat

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,4 +1,11 @@
 var colour = "#000000";
+var messages = [
+	["Bob marley", "This game ***** I hate it"],
+	["John Lemon", "Agreed, **** ******* the woman is the ****** of the world"],
+	["Saddam Hussein", "Guys let's calm down and be more peaceful this game is not that bad."],
+	["Jose", "Ay Caramba di merda puta madre"],
+	["Donald", "We should draw a wall"] 
+];
 
 function interpretClick(){
 	const canvas = document.getElementById("canvas");
@@ -20,4 +27,24 @@ function updateColour(){
 	var selection = document.querySelectorAll(":hover"); 
 	selection = selection[selection.length-1]; 
 	colour = window.getComputedStyle(selection).backgroundColor;
+}
+
+async function getMessages(){
+	var messageBox = document.getElementById("messageText");
+	var text = "";
+	//fetch /chat/getMessages/ and update the array
+	for(let i = 0; i < messages.length; i++){
+		text = "";
+		text = "<b>" + messages[i][0] + "<b> : " + messages[i][1];
+		messageBox.innerHTML += text + "<br><br>";
+	}
+}
+
+async function sendMessage(){
+	var message = document.getElementById("textInput");
+	if(message.length > 300){
+		alert("Message length above 300, be more concise");
+		return;
+	}
+
 }
