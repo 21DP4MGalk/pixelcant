a3622fea06dfa8641e2ed479e858cea72e3b2dbf
21DP4KLogi <103188972+21DP4KLogi@users.noreply.github.com>
2024-10-15 16:42:21 +0300

    Implemented login api

--- src/pixelcant.nim
+++ src/pixelcant.nim
@@ -3,7 +3,7 @@ import norm/[postgres, types, model]
 import ws, ws/jester_extra
 import dotenv
 import checksums/bcrypt
-import std/[os, strutils, json, options, locks, times]
+import std/[os, strutils, json, options, times, sysrand]
 import "models.nim"
 import "websockets.nim"
 import "api"/["auth.nim", "canvas.nim", "chat.nim", "user.nim"]
