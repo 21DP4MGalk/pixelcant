282306f9765cabac635355fc39a166a671cb9c7b
21DP4KLogi <103188972+21DP4KLogi@users.noreply.github.com>
2024-10-29 15:08:34 +0200

    Adjusted frontend

--- public/js/navbar_uni.js
+++ public/js/navbar_uni.js
@@ -20,13 +20,13 @@ async function checkStatus() {
   loginElement.style.display = "none";
   registerElement.style.display = "none";
   let status = JSON.parse(await resp.text());
-  let greeting = ""
+  let greeting = "";
   if (status.banned) {
-    greeting = "ew, "
-  } else if(status.admin) {
-    greeting = "HELLO, you're bald, "
+    greeting = "ew, ";
+  } else if (status.admin) {
+    greeting = "HELLO, you're bald, ";
   } else {
-    greeting = "WELCOME, HIGHLY REGARDED "
+    greeting = "WELCOME, HIGHLY REGARDED ";
   }
   usernameMsg.innerText = greeting + status.username + "!";
 }
